a07cde61106537a32423b15d8e9e5b09c1d0cda0
rafalgryglas <rafal.gryglas@gmail.com>
2019-07-10 18:43:06 +0200

    Enigma/Mastermind game first commit

---
+++ src/main/java/com/game/enigma/EnigmaRunner.java
@@ -0,0 +1,9 @@
+package com.game.enigma;
+
+public class EnigmaRunner {
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.enigmaPlay();
+    }
+}
---
+++ src/main/java/com/game/enigma/Game.java
@@ -0,0 +1,46 @@
+package com.game.enigma;
+
+import java.util.List;
+
+public class Game {
+
+    public boolean enigmaPlay() {
+        EnigmaBoard board = new EnigmaBoard();
+        List<Integer> computerCode = CodeMaker.computerMoves();
+        while (true) {
+            System.out.println(board);
+            if (UserDialogs.firstViewDialogs()) {
+                for (int i = 0; i < 10; i++) {
+                    List<Integer> playerCode = UserDialogs.getMove(i);
+                    board.setValue(i, 0, playerCode.get(0));
+                    board.setValue(i, 1, playerCode.get(1));
+                    board.setValue(i, 2, playerCode.get(2));
+                    board.setValue(i, 3, playerCode.get(3));
+                    board.setValue(i, 4, -1);
+                    List<Integer> compCodeResult = CodeChecker.checker(computerCode, playerCode);
+                    board.setValue(i, 5, compCodeResult.get(0));
+                    board.setValue(i, 6, compCodeResult.get(1));
+                    board.setValue(i, 7, compCodeResult.get(2));
+                    board.setValue(i, 8, compCodeResult.get(3));
+                    System.out.println(board);
+                    Integer sum = 0;
+                    for (int k = 0; k < 4; k++) {
+                        sum += board.getValue(i, 5 + k);
+                        System.out.println(sum);
+                        if (sum == 400) {
+                            i = -1;
+                            board = new EnigmaBoard();
+                            computerCode = CodeMaker.computerMoves();
+                            if (!UserDialogs.oneMoreGame()) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                System.out.println("Correct code: " + computerCode.toString());
+                UserDialogs.youLose();
+            }
+            return false;
+        }
+    }
+}
